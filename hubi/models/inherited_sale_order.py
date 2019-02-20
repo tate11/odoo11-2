0784f75488a4275f7f004db722f14b5c6a453c54
Puillet <pierreguillet99@gmail.com>
2019-02-20 09:13:53 +0100

    Update Sale Order

--- hubi/models/inherited_sale_order.py
+++ hubi/models/inherited_sale_order.py
@@ -114,7 +114,7 @@ class HubiSaleOrderLine(models.Model):
     @api.multi
     def print_label(self):
         self.filtered(lambda s: s.state == 'draft').write({'state': 'sent'})
-        return self.env.ref('hubi.action_hubi_packing_preparation').report_action(self)
+        return {'type': 'ir.actions.report','report_name': 'report_saleorder_hubi_document','report_type':"qweb-pdf"}
 		
     @api.multi
     def validation(self):
