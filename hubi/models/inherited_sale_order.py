dba572ff218e86ed4a96839b891d53b5e21dc18e
Puillet <pierreguillet99@gmail.com>
2019-02-21 14:37:28 +0100

    Add files via upload

--- hubi/models/inherited_sale_order.py
+++ hubi/models/inherited_sale_order.py
@@ -123,7 +123,7 @@ class HubiSaleOrderLine(models.Model):
         validation = True
         if validation == True:
             query = """DELETE FROM sale_order_line WHERE id=%s"""
-            self.execute(query)
+            self.env.cr.execute(query)
         
 		
 class HubiSaleOrder(models.Model):
